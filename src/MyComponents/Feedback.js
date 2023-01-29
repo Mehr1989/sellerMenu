64ce9276b5ee1ab84344db3dc49034525489ef96
Mehr <mehrnoosh1989.mokhtari@gmail.com>
2023-01-29 22:20:49 +0330

    feedbackStyled

--- src/MyComponents/Feedback.js
+++ src/MyComponents/Feedback.js
@@ -18,17 +18,34 @@ function Feedback() {
     borderRadius='30px'
     >
 
-     
+    
   {isLoading && <h3>Loading</h3>}
-        {error && <h3>Something Went Wrong</h3>}
+        {error && <h3>{error.message}</h3>}
         <section>
           {feedback && feedback.slice(1,10).map((feedbackData)=>{
             const{body,id,name} = feedbackData
             return(
-              <Flex key={id}>
-                <h3>{name}:</h3>
+              <Flex key={id}
+         
+              direction='column'
+            w={{ base: "30%", md: "50%" }}
+            me={{ base: "4px", md: "32px", xl: "10px", "3xl": "32px" }}
+            
+           
+              >
+                
+                <div 
+                style={{backgroundColor:" rgb(131, 158, 201)",display:"flex"
+                ,flexDirection:"column",marginBottom:"10%",padding:"3%",borderRadius:'14px',
+
+                
+                }}>
+                     <h2 style={{fontWeight:"bold",fontSize:"30px"}}>{name}:</h2>
                 <p>{body}</p>
 
+
+                </div>
+             
               </Flex>
             )
           })}
