70fbd9a67782bd79a3256623bfd4091fe3d8a53f
Mehr <mehrnoosh1989.mokhtari@gmail.com>
2023-01-30 14:37:31 +0330

    inventroyStyled

--- src/MyComponents/Inventory.js
+++ src/MyComponents/Inventory.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Flex } from '@chakra-ui/react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchInventory } from 'redux/slice/inventorySlice'
-import { identifier } from 'stylis'
+
 function Inventory() {
   const {isLoading,error,inventory} = useSelector((state)=> state.inventory)
   const dispatch = useDispatch()
@@ -11,12 +11,13 @@ function Inventory() {
   },[dispatch])
   return (
     
-    <Flex
+    <Flex 
   
     bgColor={"brand"}
     py={{ base: "200px", md: "180px" }}
     px={{ base: "20px", md: "32px" }}
-  
+    flexDirection="column"
+    
     >
 
     
@@ -26,18 +27,40 @@ function Inventory() {
           {inventory && inventory.slice(1,10).map((inventoryData)=>{
             const{id,title,url} = inventoryData
             return(
-              <Flex 
-          
-              direction='column'
-            w={{ base: "30%", md: "50%" }}
-            me={{ base: "4px", md: "32px", xl: "10px", "3xl": "32px" }}
+             <Flex
+             
+             
+            bgColor={"red"}
+            py={{ base: "60%", md: "5%" }}
+            px={{ base: "100%", md: "2%",xl: "10px", "3xl": "32px"  }}
+            flexDirection={"row"}
+            
+  
+    >
+      
             
            
-              >
-                
-                <div key={id}
-                style={{backgroundColor:" rgb(131, 158, 201)",display:"flex"
-                ,flexDirection:"column",marginBottom:"10%",padding:"3%",borderRadius:'14px',
+              
+                <Flex
+
+                   direction='column'
+                 w={{ base: "30%", md: "40%" }}
+                 me={{ base: "4px", md: "32px", xl: "10px", "3xl": "32px" }}
+                 
+                 bgColor={"green"}
+                 justifyContent={"center"}
+                 alignItems={"center"}
+                 borderRadius={"18px"}
+                   
+            
+                >
+
+                <div key={id} 
+                style={{display:"flex"
+               ,marginBottom:"10%",padding:"3%",borderRadius:'14px',
+               width:"70%" , height:"70%", backgroundColor:"yellow",
+               justifyContent:"center"
+               
 
                 
                 }}>
@@ -45,11 +68,14 @@ function Inventory() {
                <img src={url} alt="meal"></img>
                </div>
 
-              <h2 style={{fontWeight:"bold",fontSize:"30px"}}>{title}:</h2>
+              <h6 style={{fontWeight:"bold",fontSize:"25px"}}>{title}:</h6>
+
+                </Flex>
+                
 
               
              
-              </Flex>
+                </Flex>
             )
           })}
         </section>
